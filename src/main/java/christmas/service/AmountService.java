8060bc930d341d1b91cb251baf1b9d3506f1cc2b
song-jae-wan <sjo9810@gmail.com>
2023-11-10 22:00:19 +0900

    feat: 서비스 클래스에 인스턴스 변수 추가

--- src/main/java/christmas/service/AmountService.java
+++ src/main/java/christmas/service/AmountService.java
@@ -1,13 +1,26 @@
 package christmas.service;
 
 import christmas.domain.AmountDTO;
+import christmas.domain.BillBuilder;
+import christmas.domain.DiscountCalculator;
 
 public class AmountService {
 
     private final AmountDTO amountDTO;
+    private final DiscountCalculator discountCalculator;
+    private final BillBuilder billBuilder;
 
-    public AmountService(final AmountDTO amountDTO) {
+    public AmountService(
+            final AmountDTO amountDTO,
+            final DiscountCalculator discountCalculator,
+            final BillBuilder billBuilder) {
         this.amountDTO = amountDTO;
+        this.discountCalculator = discountCalculator;
+        this.billBuilder = billBuilder;
     }
+
+
+
+
 
 }
