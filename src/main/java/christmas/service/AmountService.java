32227166926b1cf316cd40f2cdf86e1bf185b073
song-jae-wan <sjo9810@gmail.com>
2023-11-10 21:53:31 +0900

    feat: 계산된 금액 처리할 서비스 생성

---
+++ src/main/java/christmas/service/AmountService.java
@@ -0,0 +1,13 @@
+package christmas.service;
+
+import christmas.domain.AmountDTO;
+
+public class AmountService {
+
+    private final AmountDTO amountDTO;
+
+    public AmountService(final AmountDTO amountDTO) {
+        this.amountDTO = amountDTO;
+    }
+
+}
