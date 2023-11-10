12647283af848d05672d2a3ef05dab83751ee03b
song-jae-wan <sjo9810@gmail.com>
2023-11-10 23:23:12 +0900

    test: 계산기 테스트

---
+++ src/test/java/christmas/domain/CalculatorTest.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import static christmas.domain.constants.FoodConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    private final DiscountCalculator discountCalculator = new DiscountCalculator(
+            new Day(3), new OrderMenu(Map.of(STEAK, 1, RIB, 1, CAKE, 2, COKE, 1)));
+
+    @DisplayName("크리스마스 디데이 할인 금액을 반환한다.")
+    @Test
+    void calculateChristmas() {
+        assertThat(discountCalculator.calculateChristmas()).isEqualTo(1200);
+    }
+
+    @DisplayName("평일 할인 금액을 반환한다.")
+    @Test
+    void calculateWeekDay() {
+        assertThat(discountCalculator.calculateWeekday()).isEqualTo(4046);
+    }
+
+    @DisplayName("주말 할인 금액을 반환한다.")
+    @Test
+    void calculateWeekend() {
+        assertThat(discountCalculator.calculateWeekend()).isEqualTo(4046);
+    }
+}
