b963af8f76da954195561daf29d45fea4cab1898
song-jae-wan <sjo9810@gmail.com>
2023-11-10 14:37:10 +0900

    test: 할인 적용일 테스트

--- src/test/java/christmas/domain/DayTest.java
+++ src/test/java/christmas/domain/DayTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,27 @@ public class DayTest {
     void createDayByWrongNumber(int day) {
         assertThatThrownBy(() -> new Day(day))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("크리스마스 할인이 적용된다면 true 반환")
+    @Test
+    void createDayByChristmas() {
+        assertThat(new Day(25).isChristmas())
+                .isEqualTo(true);
+    }
+
+    @DisplayName("특별 할인이 적용된다면 true 반환")
+    @Test
+    void createDayByDiscountDay() {
+        assertThat(new Day(3).isSpecialDay())
+                .isEqualTo(true);
+    }
+
+    @DisplayName("주말 할인이 적용된다면 true 반환")
+    @Test
+    void createDayByWeekend() {
+        assertThat(new Day(2).isWeekend())
+                .isEqualTo(true);
     }
 
 }
