cd1b8a00b110f27e08dbabc0fdd93559d7b9807b
song-jae-wan <sjo9810@gmail.com>
2023-11-10 13:53:45 +0900

    feat: 할인일 판단 기능 구현

--- src/main/java/christmas/domain/DiscountAmount.java
+++ src/main/java/christmas/domain/DiscountAmount.java
@@ -3,11 +3,39 @@ package christmas.domain;
 public class DiscountAmount {
 
     private final int day;
-    private final OrderMenu orderMenu;
 
-    public DiscountAmount(final int day, final OrderMenu orderMenu) {
+    // TODO: 상수처리 할것
+
+    public DiscountAmount(final int day) {
+        validateDay(day);
         this.day = day;
-        this.orderMenu = orderMenu;
+    }
+
+    private void validateDay(final int day) {
+        if(day > 31 || day < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public boolean isChristmas() {
+        return day <= 25;
+    }
+
+    private int modDay() {
+        return day % 7;
+    }
+
+    public boolean isWeekend() {
+        for(int i = 1; i < 3; i++) {
+            if(modDay() == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isSpecialDay() {
+        return modDay() == 3 || day == 25;
     }
 
 }
