ab618e2a28614b0ad3256c739eef4c76c5e66854
song-jae-wan <sjo9810@gmail.com>
2023-11-10 12:41:14 +0900

    test: 주문수 유효성 체크 기능 테스트

--- src/test/java/christmas/domain/OrderMenuTest.java
+++ src/test/java/christmas/domain/OrderMenuTest.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import static christmas.domain.constants.FoodConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -15,9 +16,17 @@ public class OrderMenuTest {
     @DisplayName("메뉴의 문자열 변환값 확인하기")
     @Test
     void createOrder() {
-        Map<FoodConstants, Integer> order = new EnumMap<>(Map.of(TAPAS, 1, COKE, 1));
+        Map<FoodConstants, Integer> order = new EnumMap<>(
+                Map.of(TAPAS, 1, COKE, 1));
+
         assertThat(new OrderMenu(order).toString())
                 .isEqualTo("타파스 1개\n제로콜라 1개\n");
     }
 
+    @DisplayName("총 주문수가 20을 초과하면 예외가 발생한다.")
+    @Test
+    void createOrderByOverCount() {
+        assertThatThrownBy(() -> new OrderMenu(Map.of(TAPAS, 21)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
