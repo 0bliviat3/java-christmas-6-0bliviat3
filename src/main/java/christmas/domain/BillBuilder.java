b0617e29b2060cfd638a89c77d33d22454c0c11c
song-jae-wan <sjo9810@gmail.com>
2023-11-11 00:04:14 +0900

    feat: 총액 계산기능 구현

--- src/main/java/christmas/domain/BillBuilder.java
+++ src/main/java/christmas/domain/BillBuilder.java
@@ -7,13 +7,33 @@ import java.text.DecimalFormat;
 public class BillBuilder {
 
     private static final String EMPTY = "";
-    private static final String DecimalREGEX = "-###,###";
+    private static final String CHAMPAGNE = "샴페인 1개\n";
+    private static final String DecimalREGEX = "###,###";
     private final DecimalFormat amountFormat;
-    private final DiscountCalculator discountCalculator;
+    private final AmountDTO amountDTO;
 
-    public BillBuilder(DiscountCalculator discountCalculator) {
+    public BillBuilder(final AmountDTO amountDTO) {
         amountFormat = new DecimalFormat(DecimalREGEX);
-        this.discountCalculator = discountCalculator;
+        this.amountDTO = amountDTO;
     }
 
+    private int calculateBenefitsAmount() {
+        int sum = amountDTO.getChristmasDiscount() +
+                amountDTO.getSpecialDiscount() +
+                amountDTO.getGiftDiscount();
+        if(amountDTO.isWeekend()) {
+            return (sum + amountDTO.getWeekendDiscount()) * -1;
+        }
+        return (sum + amountDTO.getWeekdayDiscount()) * -1;
+    }
+
+    private int calculatePay() {
+        int pay = amountDTO.getAmount() -
+                amountDTO.getChristmasDiscount() -
+                amountDTO.getSpecialDiscount();
+        if(amountDTO.isWeekend()) {
+            return pay - amountDTO.getWeekendDiscount();
+        }
+        return pay - amountDTO.getWeekdayDiscount();
+    }
 }
