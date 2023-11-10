7a5cc992e4043db08a901f637d4750fe692e347f
song-jae-wan <sjo9810@gmail.com>
2023-11-10 12:25:31 +0900

    feat: 주문받은 메뉴 문자열로 변환기능 구현

--- src/main/java/christmas/domain/OrderMenu.java
+++ src/main/java/christmas/domain/OrderMenu.java
@@ -6,10 +6,30 @@ import christmas.domain.constants.FoodConstants;
 
 public class OrderMenu {
 
+    private static final String MENU_FORMAT = "%s %d개\n";
     private final Map<FoodConstants, Integer> order;
 
     public OrderMenu(final Map<FoodConstants, Integer> order) {
         this.order = order;
     }
 
+    private void validateOrderCount(final Map<FoodConstants, Integer> order) {
+        int sum = 0;
+        for(int count : order.values()) {
+            sum += count;
+            if(sum > 20) {
+                throw new IllegalArgumentException("[ERROR] 메뉴는 최대 20개까지만 주문할 수 있습니다.");
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+       StringBuilder orderBuilder = new StringBuilder();
+       order.forEach((foodConstants, count) -> {
+           orderBuilder.append(
+                   String.format(MENU_FORMAT,foodConstants.getFood(),count));
+       });
+       return orderBuilder.toString();
+    }
 }
