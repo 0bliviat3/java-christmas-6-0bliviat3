52185ba7bb526ceac783db9225d7ba1c0c982250
song-jae-wan <sjo9810@gmail.com>
2023-11-11 14:34:05 +0900

    feat: 배지 클래스 생성

---
+++ src/main/java/christmas/domain/Badge.java
@@ -0,0 +1,12 @@
+package christmas.domain;
+
+public class Badge {
+
+    private static String MESSAGE = "<12월 이벤트 배지>\n";
+    private final int benefitAmount;
+
+    public Badge(final int benefitAmount) {
+        this.benefitAmount = benefitAmount;
+    }
+
+}
