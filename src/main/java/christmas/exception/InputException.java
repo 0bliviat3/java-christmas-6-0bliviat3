f5a5c9d4106f117076ed1ab1778396bdfa4ae2b4
song-jae-wan <sjo9810@gmail.com>
2023-11-11 21:16:09 +0900

    feat: 숫자 예외처리 구현

--- src/main/java/christmas/exception/InputException.java
+++ src/main/java/christmas/exception/InputException.java
@@ -1,4 +1,30 @@
 package christmas.exception;
 
+import static christmas.exception.ExceptionConstants.*;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import christmas.domain.constants.FoodConstants;
+
 public class InputException {
+
+    private static final String NUMBER_REGEX = "[0-9]+";
+    private static final Pattern regex = Pattern.compile(NUMBER_REGEX);
+
+    private InputException() {
+    }
+
+    public static int validateNumber(String number, ExceptionConstants exceptionConstants) {
+        if (regex.matcher(number).matches()) {
+            return Integer.parseInt(number);
+        }
+        throw createException(exceptionConstants);
+    }
+
+    public static IllegalArgumentException createException(ExceptionConstants exceptionConstants) {
+        return new IllegalArgumentException(exceptionConstants.getMessage());
+    }
 }
