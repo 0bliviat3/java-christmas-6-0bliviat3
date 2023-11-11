ca6fae1273ac75f6398f60a68b4e365a1c683e2b
song-jae-wan <sjo9810@gmail.com>
2023-11-11 14:58:34 +0900

    feat: 배지 생성기능 구현

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import static christmas.domain.constants.BadgeConstants.*;
+
+import christmas.domain.constants.BadgeConstants;
+
 public class Badge {
 
     private static String MESSAGE = "<12월 이벤트 배지>\n";
@@ -7,6 +11,19 @@ public class Badge {
 
     public Badge(final int benefitAmount) {
         this.benefitAmount = benefitAmount;
+    }
+
+    public String getBadge() {
+        return String.join(MESSAGE, buildBadge());
+    }
+
+    private String buildBadge() {
+        for(BadgeConstants badgeConstants : BadgeConstants.values()) {
+            if(badgeConstants.getAmount() <= benefitAmount) {
+                return badgeConstants.getBadge();
+            }
+        }
+        return NONE.getBadge();
     }
 
 }
