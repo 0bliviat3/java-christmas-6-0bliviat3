922aa6c03bf9011d7c2b77762683ec85b0aef809
song-jae-wan <sjo9810@gmail.com>
2023-11-12 23:40:19 +0900

    fix: 테스트반례에 따른 버그 수정
    - 반례) 초코케이크--1
    - 반례) 제로콜라,3
    - 구분자가 연속되거나 동시에 사용함에 따라 발생되는 버그 수정

--- src/main/java/christmas/exception/InputException.java
+++ src/main/java/christmas/exception/InputException.java
@@ -2,7 +2,9 @@ package christmas.exception;
 
 import static christmas.exception.ExceptionConstants.*;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
@@ -12,8 +14,10 @@ import christmas.domain.constants.FoodConstants;
 public class InputException {
 
     private static final String NUMBER_REGEX = "[0-9]+";
-    private static final String SPLIT_DELIMITER = "-,";
-    private static final int SINGLE = 1;
+    private static final String COMAS = ",";
+    private static final String BAR = "-";
+    private static final int FOOD_INDEX = 0;
+    private static final int COUNT = 1;
     private static final int PAIR = 2;
     private static final Pattern regex = Pattern.compile(NUMBER_REGEX);
 
@@ -29,11 +33,11 @@ public class InputException {
 
     public static Map<FoodConstants, Integer> validateOrder(String order) {
         Map<FoodConstants, Integer> orderMenu = new EnumMap<>(FoodConstants.class);
-        StringTokenizer orderToken = new StringTokenizer(order, SPLIT_DELIMITER);
-        validateOrder(orderToken.countTokens());
-        while (orderToken.hasMoreTokens()) {
-            FoodConstants food = FoodConstants.from(orderToken.nextToken());
-            int count = validateOrderCount(orderToken.nextToken());
+        List<String> orderList = List.of(order.split(COMAS));
+        for (String orderPiece : orderList) {
+            List<String> orderOne = validateOrderDivision(orderPiece);
+            FoodConstants food = FoodConstants.from(orderOne.get(FOOD_INDEX));
+            int count = validateOrderCount(orderOne.get(COUNT));
             if (orderMenu.containsKey(food)) {
                 throw createException(ORDER);
             }
@@ -42,18 +46,20 @@ public class InputException {
         return orderMenu;
     }
 
+    private static List<String> validateOrderDivision(String orderPiece) {
+        List<String> order = List.of(orderPiece.split(BAR));
+        if(order.size() != PAIR) {
+            throw createException(ORDER);
+        }
+        return order;
+    }
+
     private static int validateOrderCount(String orderCount) {
         int count = validateNumber(orderCount, ORDER);
-        if (count < SINGLE) {
+        if (count < COUNT) {
             throw createException(ORDER);
         }
         return count;
-    }
-
-    private static void validateOrder(int count) {
-        if (count % PAIR == SINGLE) {
-            throw createException(ORDER);
-        }
     }
 
     public static IllegalArgumentException createException(ExceptionConstants exceptionConstants) {
