f01f85bebae3edf4015a7f4aa03047e5e56e7548
song-jae-wan <sjo9810@gmail.com>
2023-11-12 19:29:28 +0900

    refactor: OutputView 상수 처리

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,21 +1,19 @@
 package christmas.view;
 
+import static christmas.view.constants.OutputConstants.*;
+
 public class OutputView {
 
-    private static final String PREVIEW = "12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
-    private static final String MENU = "\n<주문 메뉴>";
-    private static final String BADGE = "\n<12월 이벤트 배지>";
-
     public void printPreview() {
-        System.out.println(PREVIEW);
+        System.out.println(PREVIEW.getMessage());
     }
 
     public void printMenu() {
-        System.out.println(MENU);
+        System.out.println(MENU.getMessage());
     }
 
     public void printBadge() {
-        System.out.println(BADGE);
+        System.out.println(BADGE.getMessage());
     }
 
     public void printModel(String model) {
