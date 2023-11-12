28ef190c754ae01722eca11d64eedb57fd01f3e8
song-jae-wan <sjo9810@gmail.com>
2023-11-12 20:41:16 +0900

    refactor: 자바에서 제공하는 개행 함수 적용

--- src/main/java/christmas/domain/constants/DiscountConstants.java
+++ src/main/java/christmas/domain/constants/DiscountConstants.java
@@ -2,17 +2,17 @@ package christmas.domain.constants;
 
 public enum DiscountConstants {
 
-    BEFORE_DISCOUNT("\n<할인 전 총주문 금액>\n%s원\n"),
-    GIFT_MENU("\n<증정 메뉴>\n"),
-    BENEFITS_DETAILS("\n<혜택 내역>\n"),
-    CHRISTMAS("크리스마스 디데이 할인: -%s원\n"),
-    WEEKDAY("평일 할인: -%s원\n"),
-    WEEKEND("주말 할인: -%s원\n"),
-    SPECIAL("특별 할인: -%s원\n"),
-    GIFT("증정 이벤트: -%s원\n"),
-    BENEFITS_AMOUNT("\n<총혜택 금액>\n%s원\n"),
-    AFTER_DISCOUNT("\n<할인 후 예상 결제 금액>\n%s원\n"),
-    NONE("없음\n");
+    BEFORE_DISCOUNT("<할인 전 총주문 금액>\n%s원" + System.lineSeparator()),
+    GIFT_MENU("\n<증정 메뉴>" + System.lineSeparator()),
+    BENEFITS_DETAILS("\n<혜택 내역>" + System.lineSeparator()),
+    CHRISTMAS("크리스마스 디데이 할인: -%s원" + System.lineSeparator()),
+    WEEKDAY("평일 할인: -%s원" + System.lineSeparator()),
+    WEEKEND("주말 할인: -%s원" + System.lineSeparator()),
+    SPECIAL("특별 할인: -%s원" + System.lineSeparator()),
+    GIFT("증정 이벤트: -%s원" + System.lineSeparator()),
+    BENEFITS_AMOUNT("\n<총혜택 금액>\n%s원" + System.lineSeparator()),
+    AFTER_DISCOUNT("\n<할인 후 예상 결제 금액>\n%s원" + System.lineSeparator()),
+    NONE("없음" + System.lineSeparator());
 
     private final String message;
 
