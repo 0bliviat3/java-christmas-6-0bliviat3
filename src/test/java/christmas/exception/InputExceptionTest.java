107e1c8c96c2fc81a653d0ff4e64239c3137da42
song-jae-wan <sjo9810@gmail.com>
2023-11-12 23:38:44 +0900

    test: 입력예외 테스트
    - 숫자예외 태스트
    - 주문예외 테스트

---
+++ src/test/java/christmas/exception/InputExceptionTest.java
@@ -0,0 +1,27 @@
+package christmas.exception;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputExceptionTest {
+
+    @DisplayName("숫자가 아닌 다른 문자를 입력시 예외 발생")
+    @Test
+    void inputNotaNumber() {
+        assertThatThrownBy(() -> InputException.validateNumber("a", ExceptionConstants.ORDER))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("문제있는 메뉴를 입력하면 예외가 발생한다.")
+    @ValueSource(strings = {"타파스-1,타파스-2", "딤섬-20", "제로콜라-0", "초코케이크--1", "제로콜라,3"})
+    @ParameterizedTest
+    void inputOrderByProblematic(String order) {
+        assertThatThrownBy(() -> InputException.validateOrder(order))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
