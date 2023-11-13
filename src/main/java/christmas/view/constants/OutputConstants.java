fb97a2448c8366978e509e14ebea24a2e2ba9bef
song-jae-wan <sjo9810@gmail.com>
2023-11-13 10:19:49 +0900

    refactor: 줄바꿈제거

--- src/main/java/christmas/view/constants/OutputConstants.java
+++ src/main/java/christmas/view/constants/OutputConstants.java
@@ -4,7 +4,7 @@ public enum OutputConstants {
 
     PREVIEW("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
     MENU("\n<주문 메뉴>"),
-    BADGE("\n<12월 이벤트 배지>");
+    BADGE("<12월 이벤트 배지>");
 
     private final String message;
 
