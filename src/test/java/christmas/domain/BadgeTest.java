49f4f735fce831048992faa01063f7c42736ddd3
song-jae-wan <sjo9810@gmail.com>
2023-11-13 16:35:21 +0900

    test: 음수가 주어질경우 예외발생 테스트

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -1,8 +1,10 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -14,6 +16,13 @@ public class BadgeTest {
     void createBadge(int benefitAmount, String badgeConst) {
         assertThat(new Badge(benefitAmount).toString())
                 .contains(badgeConst);
+    }
+
+    @DisplayName("음수로 배지를 생성하려하면 예외가 발생한다.")
+    @Test
+    void createBadgeByNegative() {
+        assertThatThrownBy(() -> new Badge(-1000).toString())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 }
